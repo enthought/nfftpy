826f0598d7f73ed15e11aa2ff719d7e805aa416c
Jonathan March <jmarch@enthought.com>
2011-04-11 11:47:28 -0500

    Improve makefile and setup.py to build only one chosen test.
    setup.py now takes a --temp or a --simple parameter.
    makefile uses that parameter when needed.

--- makefile
+++ makefile
@@ -1,25 +1,22 @@
 # Make NFFT cython wrapper
 
-test.so: test.c
-	python setup.py build_ext --inplace
-test.c: test.pyx
-	cython test.pyx
-
-
-# trivial cythonization of simple_test.c:
-
-simple_test.so: simple_test.c
-	python setup.py build_ext --inplace
-simple_test.c: simple_test.pyx
-	cython simple_test.pyx
-
-
-# simple_test_np and nfftpy are in progress, not working yet:
+# tests in progress:
 
 simple_test_np.so: simple_test_np.c
 	python setup.py build_ext --inplace
 simple_test_np.c: simple_test_np.pyx
 	cython simple_test_np.pyx
+
+
+# trivial cythonization of simple_test.c:
+
+simple_test.so: simple_test.c
+	python setup.py build_ext --inplace --simple
+simple_test.c: simple_test.pyx
+	cython simple_test.pyx
+
+
+# nfftpy in progress, not working yet:
 
 nfftpy.so: nfftpy.c
 	python setup.py build_ext --inplace
@@ -27,18 +24,24 @@ nfftpy.c: nfftpy.pyx
 	cython nfftpy.pyx
 
 
+temp.so: temp.c
+	python setup.py build_ext --inplace --temp
+	echo "\n"
+	python -c "import temp"
+temp.c: temp.pyx
+	cython temp.pyx
+
 
 # non-build commands:
 
 test:
+	python -c "import simple_test_np"
+
+test_simple:
 	python -c "import simple_test"
 
-testtest:
-	python -c "import test"
-
-
 clean:
-	rm -f test.so test.c
+	rm -f temp.so temp.c
 	rm -f simple_test.so simple_test.c
 	rm -f simple_test_np.so simple_test_np.c
 	rm -f nfftpy.so nfftpy.c
--- setup.py
+++ setup.py
@@ -1,6 +1,13 @@
 """
 Setup extension modules for cython wrapper for NFFT.
+
+Command line parameters: build_ext --inplace, optionally followed by:
+    --temp - to build from temp.pyx
+    --simple - to build from simple_test.pyx
+    otherwise builds from simple_test_np.pyx
+
 """
+import sys
 
 from distutils.core import setup
 from distutils.extension import Extension
@@ -15,7 +22,8 @@ nfft_params = dict(
     include_dirs=[INCLUDEPATH, numpy.get_include()],
     library_dirs=[LIBPATH],
     libraries=['nfft3'],
-    # compile arguments taken from NFFT bash script 'simple_test'
+    # Compile arguments taken from NFFT bash script 'simple_test'.
+    # To see preprocessor output as .i file, insert '-save-temps':
     extra_compile_args='-O3 -fomit-frame-pointer -malign-double '
                        '-fstrict-aliasing -ffast-math'.split(),
     extra_link_args=['-Wl,-rpath='+LIBPATH]
@@ -24,11 +32,18 @@ nfft_params = dict(
 def extension(name):
     return Extension(name=name, sources=[name+'.pyx'], **nfft_params)
 
-ext_modules = [extension(name) for name in [
-    'test',
-    #'simple_test',
-    #'simple_test_np'
-    ]]
+def extmods(*names):
+    return [extension(name) for name in names]
+
+ext_modules = extmods('simple_test_np')
+if len(sys.argv) > 3:
+    extra = sys.argv[-1]
+    if extra == '--temp':
+        ext_modules = extmods('temp')
+        del sys.argv[-1]
+    elif extra == '--simple':
+        ext_modules = extmods('simple_test')
+        del sys.argv[-1]
 
 setup(
   name = 'NFFT wrapper for Python',
